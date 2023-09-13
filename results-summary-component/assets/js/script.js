6c9dc0b699ae37ab97a2e189d89192662c20d20a
metatron1986 <wilhelm.v.lenz@gmail.com>
2023-09-13 19:59:16 +0200

    added score funktionality

---
+++ results-summary-component/assets/js/script.js
@@ -0,0 +1,41 @@
+let globalData;
+const accumulatedScore = document.querySelector(".score");
+const imageIcons = document.querySelectorAll(".item-icon");
+const individualScore = document.querySelectorAll(".item-result .score");
+const categories = document.querySelectorAll(".item-category");
+
+async function fetchData() {
+  try {
+    const response = await fetch("/results-summary-component/data.json");
+    globalData = await response.json();
+    return globalData;
+  } catch (error) {
+    console.log("Fehler:", error);
+  }
+}
+
+async function main() {
+  await fetchData();
+  let category = globalData.map((item) => item.category);
+  let score = globalData.map((item) => item.score);
+  let icon = globalData.map((item) => item.icon);
+
+  for (let i = 0; i < category.length; i++) {
+    categories[i].innerText = category[i];
+  }
+
+  let result = 0;
+
+  for (let i = 0; i < score.length; i++) {
+    individualScore[i].innerText = score[i] + " ";
+    result += score[i];
+  }
+
+  accumulatedScore.innerText = Math.floor(result / individualScore.length);
+
+  for (let i = 0; i < icon.length; i++) {
+    imageIcons[i].src = icon[i];
+  }
+}
+
+main();
