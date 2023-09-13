c137d55de5543bfb19c9e05cef87112203157bde
metatron1986 <wilhelm.v.lenz@gmail.com>
2023-09-13 20:41:51 +0200

    updated fetchData and main function

--- results-summary-component/assets/js/script.js
+++ results-summary-component/assets/js/script.js
@@ -7,6 +7,9 @@ const categories = document.querySelectorAll(".item-category");
 async function fetchData() {
   try {
     const response = await fetch("/results-summary-component/data.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     globalData = await response.json();
     return globalData;
   } catch (error) {
@@ -16,25 +19,29 @@ async function fetchData() {
 
 async function main() {
   await fetchData();
-  let category = globalData.map((item) => item.category);
-  let score = globalData.map((item) => item.score);
-  let icon = globalData.map((item) => item.icon);
+  if (globalData) {
+    let category = globalData.map((item) => item.category);
+    let score = globalData.map((item) => item.score);
+    let icon = globalData.map((item) => item.icon);
 
-  for (let i = 0; i < category.length; i++) {
-    categories[i].innerText = category[i];
-  }
+    for (let i = 0; i < category.length; i++) {
+      categories[i].innerText = category[i];
+    }
 
-  let result = 0;
+    let result = 0;
 
-  for (let i = 0; i < score.length; i++) {
-    individualScore[i].innerText = score[i] + " ";
-    result += score[i];
-  }
+    for (let i = 0; i < score.length; i++) {
+      individualScore[i].innerText = score[i] + " ";
+      result += score[i];
+    }
 
-  accumulatedScore.innerText = Math.floor(result / individualScore.length);
+    accumulatedScore.innerText = Math.floor(result / individualScore.length);
 
-  for (let i = 0; i < icon.length; i++) {
-    imageIcons[i].src = icon[i];
+    for (let i = 0; i < icon.length; i++) {
+      imageIcons[i].src = icon[i];
+    }
+  } else {
+    console.log("globalData ist undefined");
   }
 }
 
